7ef2435db99b31820e7760d3fd1355bafd14c681
Becathausor <thierry.becart@telecom-paris.fr>
2021-10-18 10:09:44 +0200

    Correction eah

--- evaluation_representation.py
+++ evaluation_representation.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from time import sleep
 
 
 def context_plot(plotter):
-
     def wrapped(*args, **kwargs):
         plotter(*args)
         plt.xlabel("Budget")
@@ -27,14 +27,17 @@ def plot_runs(runs):
 
 @context_plot
 def plot_eah(eah):
-    plt.imshow(eah, cmap="Oranges")
+    plt.imshow(eah, cmap="Oranges", origin="lower")
 
 
 def create_eah(runs, nb_steps_costs=10, nb_steps_quality=10):
     eah = np.zeros((nb_steps_costs, nb_steps_quality))
 
+    # Cost borders
     cost_min = min([min(run[0]) for run in runs])
     cost_max = max([max(run[0]) for run in runs])
+
+    # Quality borders
     quality_min = min([min(run[1]) for run in runs])
     quality_max = max([max(run[1]) for run in runs])
 
@@ -42,31 +45,45 @@ def create_eah(runs, nb_steps_costs=10, nb_steps_quality=10):
         """
         Computes the index of eah corresponding to the cost
         """
-        step = (cost_max - cost_min) * 1.5 / nb_steps_costs
-        ind = cost // step
-        return ind
+        step = (cost_max - cost_min) / (nb_steps_costs - 1)
+        ind = (cost - cost_min) // step
+
+        print(f"Cost index: {ind}")
+        if ind >= nb_steps_costs:
+            print(cost)
+            print(cost_max)
+            raise Exception("Error step definition of Cost")
+
+        return int(ind)
 
     def get_ind_quality(quality):
         """
         Computes the index of eah corresponding to the quality
         """
-        step = (quality_max - quality_min) * 1.5 / nb_steps_costs
-        ind = quality // step
-        return ind
+        step = (quality_max - quality_min) / (nb_steps_quality - 1)
+        ind = (quality - quality_min) // step
 
-    for run in runs:
+        print(f"Quality index: {ind}")
+        if ind >= nb_steps_quality:
+            raise Exception("Error step definition of Quality")
+
+        return int(ind)
+
+    for number, run in enumerate(runs):
         eah_run = np.zeros((nb_steps_costs, nb_steps_quality))
         run_points = zip(*run)
         for point in run_points:
             # Find the edge of the histogram
-            point_tab = (get_ind_cost(point[0]), get_ind_quality(point[1]))
+            cost_ind, quality_ind = (get_ind_cost(point[0]), get_ind_quality(point[1]))
+            point_tab = cost_ind, quality_ind
 
             # Update of the run_eah
-            for i in range(int(point_tab[1]), nb_steps_quality):
-                for j in range(int(point_tab[0]), nb_steps_costs):
+            for i in range(quality_ind):
+                for j in range(cost_ind):
                     eah_run[i, j] = 1
-        eah += eah_run
 
+        eah += eah_run
+    eah = np.abs(eah - np.max(eah))
     return eah
 
 
