6e347cafeb519f9d8f9283550e84230c78923e9a
Becathausor <thierry.becart@telecom-paris.fr>
2021-10-18 09:14:49 +0200

    main tests

--- test.py
+++ test.py
@@ -8,10 +8,12 @@ import evaluation_representation
 if __name__ == "__main__":
     N_RUNS = 10
     METHOD_NAME = "num_annealing"
-    get_evaluation.create_evaluation(N_RUNS, METHOD_NAME)
+    RESOLUTION = 100
+    # get_evaluation.create_evaluation(N_RUNS, METHOD_NAME)
     runs = get_evaluation.get_data(METHOD_NAME)
     evaluation_representation.plot_runs(runs, title=METHOD_NAME)
 
-    eah = evaluation_representation.create_eah(runs, 100, 100)
+    eah = evaluation_representation.create_eah(runs, RESOLUTION, RESOLUTION)
     evaluation_representation.plot_eah(eah, title=METHOD_NAME + "_eah")
+    print(eah)
 
