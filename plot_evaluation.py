1ddb5dd966ca0b5299c9f2902f1ae3a0bd0e755a
Becathausor <thierry.becart@telecom-paris.fr>
2021-10-25 10:39:43 +0200

    Plotting of all the evaluation curves.

---
+++ plot_evaluation.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from time import sleep
+
+
+def context_plot(plotter):
+    def wrapped(*args, x_label="Budget", y_label="Quality", title="", **kwargs):
+        plotter(*args, **kwargs)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title)
+        plt.show()
+        return None
+
+    return wrapped
+
+
+@context_plot
+def plot_runs(runs):
+    """
+    Plot every runs on the same figure
+    """
+    for run in runs:
+        costs, qualities = run
+        plt.plot(costs, qualities)
+
+
+@context_plot
+def plot_eah(eah):
+    plt.imshow(eah, cmap="Oranges", origin="lower")
+
+
+@context_plot
+def plot_ert(ert):
+    plt.plot(ert)
+
+
+@context_plot
+def plot_eaf(X, Y, eaf):
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_surface(X, Y, eaf,
+                           linewidth=0, cmap=cm.coolwarm, antialiased=False)
+    fig.colorbar(surf)
