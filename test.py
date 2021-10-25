dfdb682f38cde3a9f3c791b61b716be135aeba06
Becathausor <thierry.becart@telecom-paris.fr>
2021-10-25 11:49:03 +0200

    Try to compare methods

--- test.py
+++ test.py
@@ -4,12 +4,15 @@ Plot the distribution of the algorithms run in snp.py for evaluation
 import get_evaluation
 import evaluation_representation
 import plot_evaluation
+import algo_comparison
 
 if __name__ == "__main__":
     N_RUNS = 10
     METHOD_NAME = "num_annealing"
     RESOLUTION = 100
     DELTA = 650
+    DELTAS = (600, 660, 675)
+    METHOD_NAMES = ["num_annealing", "bit_annealing"]
 
     # get_evaluation.create_evaluation(N_RUNS, METHOD_NAME)
     runs = get_evaluation.get_data(METHOD_NAME)
@@ -27,5 +30,7 @@ if __name__ == "__main__":
 
     # Plot eaf
     costs, qualities, eaf = evaluation_representation.create_eaf(runs)
-    plot_evaluation.plot_eaf(costs, qualities, eaf, title=METHOD_NAME + f"_eaf", y_label="Delta")
-    
+    plot_evaluation.plot_eaf(costs, qualities, eaf, title=METHOD_NAME + f"_eaf", y_label="Quality")
+
+    # Plot erts
+    algo_comparison.compare_ert(METHOD_NAMES, nb_runs=N_RUNS, deltas=DELTAS)
