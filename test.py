70f4cdb016163476ae1c7a0eb8f889f49402ab7b
Becathausor <thierry.becart@telecom-paris.fr>
2021-11-09 17:42:46 +0100

    Correction de l'eaf pour avoir les informations des intervalles considérés sur les axes

--- test.py
+++ test.py
@@ -18,32 +18,49 @@ if __name__ == "__main__":
     METHOD_NAMES = ["num_annealing", "bit_annealing", "num_genetical", "bit_genetical"]
     PARAMETERS = [{'nb-sensors': 3, 'sensor-range': 0.3, 'domain-width': 30},
                   {'nb-sensors': 4, 'sensor-range': 0.3, 'domain-width': 30}]
-    BUDGET = 60
+    BUDGET = 90
+    # CHOOSE WHAT TO RUN
+    RUN_CREATE_DATA = False
+    PLOT_TRAJECTORIES = False
+    RUN_EAF = False
+    RUN_ERT = False
+    RUN_BENCH = True
 
-    get_evaluation.create_evaluation(N_RUNS, method_name=METHOD_NAME)
+    if RUN_CREATE_DATA:
+        get_evaluation.create_evaluation(N_RUNS, method_name=METHOD_NAME)
+
     print(f"GATHER DATA AROUND THE {METHOD_NAME}")
     runs = get_evaluation.get_data(METHOD_NAME)
 
-    # Visualize different trajectories
-    print(f"PLOT TRAJECTORIES")
-    plot_evaluation.plot_runs(runs, title=METHOD_NAME)
+    if PLOT_TRAJECTORIES:
+        # Visualize different trajectories
+        print(f"PLOT TRAJECTORIES")
+        plot_evaluation.plot_runs(runs, title=METHOD_NAME)
 
-    # Plot eaf
-    print("PLOT EAF")
-    eaf = evaluation_representation.create_eaf(runs, RESOLUTION, RESOLUTION)
-    plot_evaluation.plot_eaf(eaf, title=METHOD_NAME + "_eaf")
+    if RUN_ERT:
+        # Plot ert according to the value of DELTA
+        print("PLOT ERT")
+        ert = evaluation_representation.create_ert(runs, DELTA)
+        plot_evaluation.plot_ert(ert, title=METHOD_NAME + f"_ert, delta={DELTA}", y_label=f"P(Quality < {DELTA})")
 
-    # Plot ert according to the value of DELTA
-    print("PLOT ERT")
-    ert = evaluation_representation.create_ert(runs, DELTA)
-    plot_evaluation.plot_ert(ert, title=METHOD_NAME + f"_ert, delta={DELTA}", y_label=f"P(Quality < {DELTA})")
+    if RUN_EAF:
+        # Plot eaf
+        print("PLOT EAF")
+        eaf, extent = evaluation_representation.create_eaf(runs, RESOLUTION, RESOLUTION)
+        plot_evaluation.plot_eaf(eaf, extent=extent, title=METHOD_NAME + "_eaf",
+                                 x_label=f"Budget [{extent[0]};{extent[1]}]",
+                                 y_label=f"Quality [{extent[2]};{extent[3]}]")
 
+    if RUN_BENCH:
+        bench = ExperimentDesign.ExperimentDesign(METHOD_NAMES, PARAMETERS, BUDGET, N_RUNS)
+        medians = bench.evaluate_bench_median()
+        print(medians)
+
+
+"""
     # Plot erts
     print("PLOT ERT")
     algo_comparison.compare_ert(METHOD_NAMES, nb_runs=N_RUNS, deltas=DELTAS,
                                 title="Comparison of algorithms", y_label="P[x > delta]",
                                 name_save="")
-"""
-    bench = ExperimentDesign.ExperimentDesign(METHOD_NAMES, PARAMETERS, BUDGET, N_RUNS)
-    bench.evaluate_bench_median()
 """
